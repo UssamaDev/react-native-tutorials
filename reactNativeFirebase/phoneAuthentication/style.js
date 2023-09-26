4af76d553a04933672efc5fc5854ea73a475b3ba
UssamaDev <144179222+UssamaDev@users.noreply.github.com>
2023-09-26 15:51:49 +0100

    Create style.js

---
+++ reactNativeFirebase/phoneAuthentication/style.js
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      borderColor: '#050f7c',
+      borderRadius: 15,
+      padding: 10,
+      backgroundColor: '#ffffff',
+      color: '#050f7c',
+      textAlign: 'center',
+      fontSize: 20,
+      width: '80%',
+    },
+    button: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 4,
+      paddingHorizontal: 16,
+      margin: 6,
+      borderRadius: 15,
+      elevation: 3,
+      backgroundColor: '#050f7c',
+      width: '80%',
+    },
+    text: {
+      color: '#050f7c',
+      fontSize: 20,
+    },
+    title: {
+      padding: 25,
+      color: '#050f7c',
+      textAlign: 'center',
+      fontSize: 35,
+      fontWeight: 'bold',
+    },
+    buttonText: {
+      padding: 10,
+      color: '#ffffff',
+      textAlign: 'center',
+      fontSize: 20,
+    },
+  });
