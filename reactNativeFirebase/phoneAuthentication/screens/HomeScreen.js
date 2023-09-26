fcaec0166e49eb547e04ca04a995de25d827dea7
UssamaDev <144179222+UssamaDev@users.noreply.github.com>
2023-09-26 15:50:11 +0100

    Create HomeScreen.js

---
+++ reactNativeFirebase/phoneAuthentication/screens/HomeScreen.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { Pressable, View, Text } from 'react-native';
+import { styles } from '../style';
+import auth from '@react-native-firebase/auth';
+
+const HomeScreen = ({navigation}) => {
+    const [user, setUser] = useState(null)
+    const signOut = () => {
+        auth().signOut();
+    };
+    useEffect(() => {
+        const subscriber = auth().onAuthStateChanged((user) => {
+          if (!user) {
+            // User is signed out
+          } else {
+            // User is signed in, you can update your state here if needed
+            setUser(user);
+          }
+        });
+        return subscriber; // unsubscribe on unmount
+      }, []);
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={styles.title}>Home Screen</Text>
+            <Text style={styles.text}>Hi {user?.phoneNumber || "loading"}</Text>
+            <Pressable
+                style={styles.button}
+                onPress={() => signOut()}>
+                <Text style={styles.buttonText}> Sign Out</Text>
+            </Pressable>
+        </View>
+    );
+}
+
+export default HomeScreen
